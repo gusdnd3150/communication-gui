402e4300d27839a90f816619d55848d43a3083da
HYEONWOONG\gusdn <gusdnd3150@gmail.com>
2024-05-02 17:47:38 +0900

    테이블 데이터 생성 로직 추가

--- src/component/settings/Settings.py
+++ src/component/settings/Settings.py
@@ -24,7 +24,6 @@ def resource_path(relative_path):
 class Settings():
     instance = None
     saveSkWindow = None
-
     sokcetList = []
 
     def __init__(self):
@@ -54,18 +53,18 @@ class Settings():
 
     def loadData(self):
         self.sokcetList = self.loadJsonFile('TB_SK_PKG_SK')
-        logger.info(self.sokcetList)
+        self.setTableData(self.sokcetList)
+
+
+    def setTableData(self, list):
         # 데이터의 행과 열 수를 얻습니다.
-        rows = len(self.sokcetList)
-        cols = len(self.sokcetList[0]) if rows > 0 else 0
+        rows = len(list)
+        cols = len(list[0]) if rows > 0 else 0
 
         self.instance.list_sk.setRowCount(rows)  # Table의 행을 설정, list의 길이
         self.instance.list_sk.setColumnCount(cols)
-
+        self.instance.list_sk.setHorizontalHeaderLabels(list[0].keys())
         # 데이터를 테이블에 삽입
         for i, row in enumerate(self.sokcetList):
-            logger.info(row)
             for j, item in enumerate(row):
-                logger.info(item)
                 self.instance.list_sk.setItem(i, j, QTableWidgetItem(row[item]))
-
