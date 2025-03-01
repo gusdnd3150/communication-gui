d1aa8c6b0a6b4f07fb399d5372f7c1b861c0d2c7
DESKTOP-7IND0FM\강현웅 <gusdnd3150@gmail.com>
2025-03-01 14:05:38 +0900

    구동시 run 경로 프린트

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import sys
+import sys, os
 sys.path.append('.')
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication
 from src.init import InitClass
 
 if __name__ == '__main__':
 
     print(f'sys.implementation.name  :: {sys.implementation.name}')
+    print(os.getcwd())
+    print(os.path.abspath('.'))
 
     app = QApplication(sys.argv)
     window = InitClass()
